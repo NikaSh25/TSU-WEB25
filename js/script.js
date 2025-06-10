ca4b8815f5444284b0f2310f00287715457ed00d
NikaSh25 <205172187+NikaSh25@users.noreply.github.com>
2025-06-10 17:38:16 +0400

    Login page and JS seperation

--- js/script.js
+++ js/script.js
@@ -13,39 +13,11 @@ pages.forEach((page) => {
   }
 });
 
-document.querySelectorAll(".course-list__filter-btn").forEach((button) =>
-  button.addEventListener("click", () => {
-    const filter = button.getAttribute("data-filter");
-    const courses = document.querySelectorAll(
-      ".course-list__cards .course-list__item"
-    );
-
-    document.querySelectorAll(".course-list__filter-btn").forEach((button) => {
-      button.classList.remove("course-list__filter-btn--active");
-    });
-
-    button.classList.add("course-list__filter-btn--active");
-
-    courses.forEach((card) => {
-      const category = card.getAttribute("data-category");
-      const hide =
-        filter.toLowerCase() !== "all" &&
-        category.toLowerCase() !== filter.toLowerCase();
-      if (hide) {
-        card.classList.add("hidden");
-      } else {
-        card.classList.remove("hidden");
-      }
-    });
-  })
-);
-
 document
   .querySelector(".header__nav-hamburger")
   .addEventListener("click", () => {
     const dropdown_list = document.querySelector(".header__nav");
     dropdown_list.classList.toggle("header__nav--dropdown");
-    console.log("hello");
   });
 
 window.addEventListener("scroll", () => {
